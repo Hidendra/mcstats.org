7f034d6a2205e9578549b4e0e4dd5123f364c3d8
Tyler Blair <hidendra@griefcraft.com>
2013-10-27 21:52:15 -0300

    readd blag url

--- www/mcstats.org/private_html/assets/template/footer.php
+++ www/mcstats.org/private_html/assets/template/footer.php
@@ -6,7 +6,7 @@
         <p> MCStats service created & maintained by <a href="mailto:hidendra@mcstats.org">Hidendra</a>. Plugins are
             owned by their respective authors. </p>
 
-        <p><a href="http://github.com/Hidendra/mcstats.org"><i class="icon-github"></i> github</a> | <a href="irc://irc.esper.net/metrics"><i class="icon-comments"></i> irc.esper.net #metrics</a></p>
+        <p><a href="http://github.com/Hidendra/mcstats.org"><i class="icon-github"></i> github</a> | <a href="http://blog.mcstats.org"><i class="icon-quote-left"></i> blog</a> | <a href="irc://irc.esper.net/metrics"><i class="icon-comments"></i> irc.esper.net #metrics</a></p>
     </div>
 </div>
 
